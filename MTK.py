a87825e386ac2ef3752af63396928ecad7f06c92
Jrieee <rama32886@gmail.com>
2024-06-19 13:11:38 +0700

    membuat class tugas

--- MTK.py
+++ MTK.py
@@ -1,3 +1,9 @@
 import csv
 import tkinter as tk
 from tkinter import ttk, messagebox
+
+class Tugas:
+    def __init__(self, id_tugas, nama_tugas, mata_kuliah):
+        self.id_tugas = int(id_tugas)
+        self.nama_tugas = nama_tugas
+        self.mata_kuliah = mata_kuliah
