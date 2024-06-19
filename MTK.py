6b654ed2bcf88883deba5546e405ed09c7239d50
Jrieee <rama32886@gmail.com>
2024-06-19 13:19:53 +0700

    menambahkan fungsi

--- MTK.py
+++ MTK.py
@@ -34,3 +34,34 @@ class ManajemenTugas:
                 self.tugas.remove(tugas)
                 return True
         return False
+    
+    def cari_tugas(self, kata_kunci, berdasarkan):
+        hasil = []
+        for tugas in self.tugas:
+            if berdasarkan == "Nama Tugas" and kata_kunci.lower() in tugas.nama_tugas.lower():
+                hasil.append(tugas)
+            elif berdasarkan == "Mata Kuliah" and kata_kunci.lower() in tugas.mata_kuliah.lower():
+                hasil.append(tugas)
+            elif berdasarkan == "ID" and kata_kunci == tugas.id_tugas:
+                hasil.append(tugas)
+        return hasil
+
+    def urutkan_tugas(self, berdasarkan):
+        if berdasarkan == "Mata Kuliah":
+            self.tugas.sort(key=lambda x: x.mata_kuliah)
+        elif berdasarkan == "ID":
+            self.tugas.sort(key=lambda x: x.id_tugas)
+
+    def simpan_tugas_ke_csv(self, filename):
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['ID', 'Nama Tugas', 'Mata Kuliah'])
+            for tugas in self.tugas:
+                writer.writerow([tugas.id_tugas, tugas.nama_tugas, tugas.mata_kuliah])
+
+    def muat_tugas_dari_csv(self, filename):
+        with open(filename, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                tugas_baru = Tugas(row['ID'], row['Nama Tugas'], row['Mata Kuliah'])
+                self.tugas.append(tugas_baru)
